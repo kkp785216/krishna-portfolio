1f2005cfac69df3fdeb42bdc0f41451646c1c36a
KRisHnA <kkp785216@gmail.com>
2022-03-06 20:19:48 +0530

    Made a FULL STACK MERN Developer Portfolio

---
+++ js/main.js
@@ -0,0 +1,69 @@
+// Append the menu for mobile
+let menu = document.getElementById('menu');
+let contact = document.getElementById('contact');
+let mobMenu = document.querySelector('#mobMenus .mobMenu');
+let newMenu = document.createElement('div');
+let newContact = document.createElement('div');
+newMenu.innerHTML = menu.innerHTML;
+newContact.innerHTML = contact.innerHTML;
+mobMenu.appendChild(newMenu.firstElementChild);
+mobMenu.appendChild(newContact.firstElementChild);
+
+
+// Menu Toggle Open and Close
+let menuOpen = document.getElementById('menuOpen');
+let menuClose = document.getElementById('menuClose');
+let menuOverlay = document.getElementById('menuOverlay');
+menuOpen.addEventListener('click', () => {
+    mobMenu.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+})
+menuClose.addEventListener('click', () => {
+    mobMenu.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+})
+menuOverlay.addEventListener('click', () => {
+    mobMenu.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+})
+
+// Text Write Animation
+let canMake = document.querySelector('.can-make ul');
+Array.from(canMake.children)
+function iterator(element) {
+    nextIndex = 0;
+    return {
+        next: function () {
+            if (nextIndex < element.length) {
+                return {
+                    value: element[nextIndex++],
+                    done: false
+                }
+            }
+            else {
+                nextIndex = 0;
+                return {
+                    done: true
+                }
+            }
+        }
+    }
+}
+
+// Itrate the website text
+let websiteItrate = iterator(canMake.children);
+let websiteItrateValue;
+let nextTime = 1;
+
+function Update() {
+    websiteItrateValue = websiteItrate.next().value;
+    if (websiteItrateValue !== undefined) {
+        websiteItrateValue.style.opacity = 1;
+    }
+    else {
+        Array.from(canMake.children).forEach((element) => {
+            element.style.opacity = 0;
+        });
+    }
+}
+setInterval(()=>{Update()}, 150);
